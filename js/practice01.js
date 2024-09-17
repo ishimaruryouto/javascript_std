3380ca201932176ad0b13e8f350bbe3e267f51ae
石丸　椋士 <>
2024-09-17 14:22:15 +0900

    電卓完成

--- js/practice01.js
+++ js/practice01.js
@@ -1,2 +1,13 @@
 // 電卓作成
-document.getElementById("number").style.display="flex";
+const num1value=document.getElementById("num1");
+const num2value=document.getElementById("num2");
+const ansvalue=document.getElementById("ans");
+
+const btn = document.getElementById("btn");
+
+
+btn.addEventListener("click", () => {
+    console.log(num1value.value);
+    console.log(num2value.value);
+    ansvalue.value=Number(num1value.value)+Number(num2value.value);
+});
