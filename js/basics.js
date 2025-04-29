ffa0d1f6ab402c710b3b9749c1f576f249b4f930
ishimaruryouto <2240006@ecc.ac.jp>
2025-04-29 14:42:08 +0900

    コード途中

---
+++ js/basics.js
@@ -0,0 +1,37 @@
+// alert("hello world");
+console.log("hello world");
+
+const foo = 1 + 1;
+console.log(foo);
+
+console.log(1 + 2);
+
+const result = 1 + 2;
+console.log(result);
+
+let myPokemon = "ピカチュウ";
+myPokemon = "ニャオハ";
+myPokemon = "ホゲータ";
+console.log(myPokemon);
+
+console.log(typeof "ピカチュウ");
+console.log(typeof 10);
+console.log(typeof true);
+console.log(typeof "null");
+
+console.log("ピカチュウ" + "ライチュウ");
+
+console.log("ピカチュウ" + "lev" + 10);
+
+const pokemonName = ["ニャオハ", "ホゲータ", "クワッス"];
+console.log(pokemonName[1]);
+
+const myMoney = 1000;
+let isShipping = false;
+if (myMoney >= 500) {
+    isShipping = true;
+}
+
+console.log(isShipping);
+
+
